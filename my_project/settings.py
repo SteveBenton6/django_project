fcec504eb1b2fdd2025359c985675e4a77ac789e
Steven Benton <steve@bentonemail.uk>
2024-07-18 12:53:17 +0000

    Doc: Update DEBUG to true.

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=b2t4_hus_i7=x@!(=l6@fp!dtw64a7xbzr&$+y+c@3cz+q50)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-stevebenton-djangoproje-pefkicuq243.ws.codeinstitute-ide.net','.herokuapp.com']
 
